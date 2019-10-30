cf996b1b48dbb165f950b1e09c29f786f6e939ab
sujata-shyam <sujata.shyam.123@gmail.com>
2019-10-30 16:24:25 +0530

    Separate dictionaries for constants and functions. Tested successfully for pi

--- interpretLISP.playground/Contents.swift
+++ interpretLISP.playground/Contents.swift
@@ -16,50 +16,52 @@ extension Double
     }
 }
 
-
 //typealias Methods = ((inout [Double]) -> Any?)
-
 //var operationsDictionary = [String:Methods]()
 //var operationsDictionary = [String:(inout [Double]) -> Any?]()
 //var operationsDictionary = [String:(arrOperands:inout [Double]) -> Any?]()
 
-var operationsDictionary = [String:([Double]) -> Any?]()
+var globalEnvFunc = [String:([Double]) -> Any?]()
 
-operationsDictionary["+"] = {return ($0.reduce(0, {$0 + $1}))}
-operationsDictionary["*"] = {return ($0.reduce(1, {$0 * $1}))}
+globalEnvFunc["+"] = {return ($0.reduce(0, {$0 + $1}))}
+globalEnvFunc["*"] = {return ($0.reduce(1, {$0 * $1}))}
 
-operationsDictionary[">"] = {return ($0[0] > $0[1]) }
-operationsDictionary["<"] = {return ($0[0] < $0[1]) }
-operationsDictionary[">="] = {return ($0[0] >= $0[1]) }
-operationsDictionary["<="] = {return ($0[0] <= $0[1]) }
+globalEnvFunc[">"] = {return ($0[0] > $0[1]) }
+globalEnvFunc["<"] = {return ($0[0] < $0[1]) }
+globalEnvFunc[">="] = {return ($0[0] >= $0[1]) }
+globalEnvFunc["<="] = {return ($0[0] <= $0[1]) }
 
-operationsDictionary["="] = {return ($0[0] == $0[1]) }
-operationsDictionary["==="] = {return ($0[0] == $0[1]) }
-operationsDictionary["!=="] = {return ($0[0] != $0[1]) }
+globalEnvFunc["="] = {return ($0[0] == $0[1]) }
+globalEnvFunc["==="] = {return ($0[0] == $0[1]) }
+globalEnvFunc["!=="] = {return ($0[0] != $0[1]) }
 
-operationsDictionary["-"] = {(arrOperands: [Double]) -> Double? in
+globalEnvFunc["-"] = {(arrOperands: [Double]) -> Double? in
     var newOperands = arrOperands
     return newOperands.count > 1 ? newOperands.dropFirst().reduce(newOperands.removeFirst(), {$0 - $1} ) : newOperands[0] * -1 }
 
-operationsDictionary["/"] = {(arrOperands: [Double]) -> Double in
+globalEnvFunc["/"] = {(arrOperands: [Double]) -> Double in
     var newOperands = arrOperands
     return (newOperands.count == 1 ? 1.0/newOperands[0] : newOperands.dropFirst().reduce(newOperands.removeFirst(), {$0 / $1})).roundTo(places: 2)  }
 
-operationsDictionary["sqrt"] = { return ($0[0].squareRoot().roundTo(places: 2)) }
+globalEnvFunc["sqrt"] = { return ($0[0].squareRoot().roundTo(places: 2)) }
 
-operationsDictionary["abs"] = { return abs($0[0]) }
+globalEnvFunc["abs"] = { return abs($0[0]) }
 
-operationsDictionary["expt"] = { return pow($0[0], $0[1]) }
+globalEnvFunc["expt"] = { return pow($0[0], $0[1]) }
 
-operationsDictionary["max"] = {return max($0[0], $0[1]) }
+globalEnvFunc["max"] = {return max($0[0], $0[1]) }
 
-operationsDictionary["min"] = {return min($0[0], $0[1]) }
+globalEnvFunc["min"] = {return min($0[0], $0[1]) }
 
-operationsDictionary["round"] = { return round($0[0]) }
+globalEnvFunc["round"] = { return round($0[0]) }
 
-operationsDictionary["pi"] = { return (Double.pi) }
+globalEnvFunc["print"] = { return(print($0)) }
 
-operationsDictionary["print"] = { return(print($0)) }
+
+var globalEnvCons = [String:Double]()
+
+globalEnvCons["pi"] = { return Double.pi }()
+
 
 //operationsDictionary["-"] = {
 //    return ($0.count > 1 ? $0.dropFirst().reduce($0.removeFirst(), {$0[0] - $0[1]} ) : $0[0] * -1) }
@@ -105,8 +107,6 @@ operationsDictionary["print"] = { return(print($0)) }
 //operationsDictionary["pi"] = {(Any)->Double in
 //    return Double.pi
 //}
-
-
 
 func numberParser(input: String) -> [Any?]?
 {
@@ -259,97 +259,167 @@ func numberParser(input: String) -> [Any?]?
     return [Double(intArray.joined()), String(inputArray)]
 }
 
-func evaluater( _ input: String)->[Any]?
+
+//func evaluater( _ input: String)->[Any]?
+//{
+//    if (input.trimmingCharacters(in: .whitespacesAndNewlines).hasPrefix("("))
+//    {
+//        var inputArray = input.split(separator: " ")//convert to array for easier operation
+//        //print(inputArray)
+//
+//        //remove the initial "("
+//        inputArray[0].count > 1 ? String(inputArray[0].removeFirst()) : String(inputArray.remove(at: 0))
+//
+//        //if () found
+//        if(inputArray[0] == ")"){return nil}
+//
+//        var resultArray = [Double]()
+//        //var argArray = [Any]()
+//        var argArray = [Double]()
+//
+//        var operation = ""
+//
+//        for _ in 0..<inputArray.count
+//        {
+//            if(inputArray[0].first == "(")
+//            {
+//                let result = evaluater(inputArray.joined(separator: " ")) //recursive call for nested exp.
+//
+//                inputArray = result![0] as! [String.SubSequence]
+//                resultArray.append(result![1] as! Double)
+//
+//                //Final operation of results got for the inner ()
+//                if ( (operation != "") && (inputArray.count == 0) && (resultArray.count > 0))
+//                {
+//                    argArray = resultArray + argArray //moving the results as args for the final op.
+//                    break
+//                }
+//            }
+//            else if(operationsDictionary.keys.contains(String(inputArray[0])))
+//            {
+//                operation = String(inputArray[0])
+//                inputArray.remove(at: 0)
+//            }
+//            else if let result =  numberParser(input: String(inputArray[0]))
+//            {
+//                inputArray.remove(at: 0)
+//                //argArray.append(result[0] as Any)
+//                argArray.append(result[0] as! Double)
+//                let remString = result[1] as! String
+//
+//                if(remString.contains(")"))// for ")"
+//                {
+//                    break
+//                }
+//            }
+//            else if (inputArray[0].contains(")"))// for non-numeric cases like ")(", ")(+", etc.
+//            {
+//
+//                if(inputArray[0].hasPrefix(")"))
+//                {
+//                    inputArray[0].removeFirst() // remove ")"
+//                    break
+//                }
+////                else if(inputArray[0].hasSuffix(")"))
+////                {
+////                    inputArray[0].removeLast() // remove ")"
+////                    break
+////                }
+//            }
+//        }
+//        return [inputArray, operationsDictionary[operation]!(argArray) as Any]
+//        //return [inputArray, operationsDictionary[operation]!(&argArray) as Any]
+//    }
+//    return nil
+//}
+
+func evaluateExp( _ input: String)->[Any]?
 {
     if (input.trimmingCharacters(in: .whitespacesAndNewlines).hasPrefix("("))
     {
-        var inputArray = input.split(separator: " ")//convert to array for easier operation
+        var inputArray = input.replacingOccurrences(of: "(", with: " ( ").replacingOccurrences(of: ")", with: " ) ").split(separator: " ")//convert to array for easier operation
+        
         //print(inputArray)
+        inputArray.remove(at: 0) //remove the initial "("
+        if(inputArray.last == ")") //remove the final ")"
+        {
+            inputArray.removeLast()
+        }
         
-        //remove the initial "("
-        inputArray[0].count > 1 ? String(inputArray[0].removeFirst()) : String(inputArray.remove(at: 0))
-        
-        //if () found
-        if(inputArray[0] == ")"){return nil}
+        if(inputArray[0] == ")"){ return nil } //if () found
         
         var resultArray = [Double]()
-        //var argArray = [Any]()
         var argArray = [Double]()
-        
+
         var operation = ""
         
         for _ in 0..<inputArray.count
         {
             if(inputArray[0].first == "(")
             {
-                let result = evaluater(inputArray.joined(separator: " ")) //recursive call for nested exp.
-            
+                let result = evaluateExp(inputArray.joined(separator: " ")) //recursive call for nested exp.
+                
+                //print("result:\(result)")
                 inputArray = result![0] as! [String.SubSequence]
                 resultArray.append(result![1] as! Double)
-                
+                //print("resultArray: \(resultArray)")
+                //print("inputArray.count: \(inputArray.count)")
                 //Final operation of results got for the inner ()
                 if ( (operation != "") && (inputArray.count == 0) && (resultArray.count > 0))
+                //if ( (operation != "") && (inputArray.count == 1) && (resultArray.count > 0))
                 {
                     argArray = resultArray + argArray //moving the results as args for the final op.
+                    //inputArray.remove(at: 0)
+                    //print("argArray:\(argArray)")
                     break
                 }
             }
-            else if(operationsDictionary.keys.contains(String(inputArray[0])))
+            else if(globalEnvFunc.keys.contains(String(inputArray[0])))
             {
                 operation = String(inputArray[0])
+                inputArray.remove(at: 0)
+            }
+            else if(globalEnvCons.keys.contains(String(inputArray[0])))
+            {
+                argArray.append(globalEnvCons[String(inputArray[0])]!)
                 inputArray.remove(at: 0)
             }
             else if let result =  numberParser(input: String(inputArray[0]))
             {
                 inputArray.remove(at: 0)
-                //argArray.append(result[0] as Any)
                 argArray.append(result[0] as! Double)
-                let remString = result[1] as! String
-                
-                if(remString.contains(")"))// for ")"
-                {
-                    break
-                }
             }
-            else if (inputArray[0].contains(")"))// for non-numeric cases like ")(", ")(+", etc.
+            else if (inputArray[0] == ")")//
             {
-                if(inputArray[0].hasPrefix(")"))
-                {
-                    inputArray[0].removeFirst() // remove ")"
-                    break
-                }
-//                else if(inputArray[0].hasSuffix(")"))
-//                {
-//                    inputArray[0].removeLast() // remove ")"
-//                    break
-//                }
+                inputArray.remove(at: 0)
+                break
             }
         }
-        return [inputArray, operationsDictionary[operation]!(argArray) as Any]
-        //return [inputArray, operationsDictionary[operation]!(&argArray) as Any]
+        return [inputArray, globalEnvFunc[operation]!(argArray) as Any]
+        //return [inputArray, globalEnvFunc[operation]!(&argArray) as Any]
+
     }
     return nil
 }
 
+//let res111 = evaluateExp("(print(+ (* 2 3 )( + 45 5)))")
+//let res11 = evaluateExp("(+ (* 2 3 )( + 45 5))")
+//let res143 = evaluateExp("(* pi 4 3)")
+//let res12 = evaluateExp("( * 2 3)")
+//let res13 = evaluateExp("( + 45 5)")
+//let res14 = evaluateExp("( * )")
+//let res141 = evaluateExp("( - 45 )")
+//let res145 = evaluateExp("( - 45 40)")
+//let res142 = evaluateExp("(/ 8 4 2)")
+//let res14_2 = evaluateExp("(/ 8)")
+//let res144 = evaluateExp("(print 4)")
+//
+//let res15 = evaluateExp("(> 45 5)")
+//let res16 = evaluateExp("(<= 44 45)")
+//let res17 = evaluateExp("(   sqrt  49)   ")
+//let res18 = evaluateExp("(+ 10 (sqrt 100))")
+//let res19 = evaluateExp("(+ (+ 4 5) (- 16 4))")
+//let res20 = evaluateExp("(abs -10.5)")
+//let res21 = evaluateExp("(!== 45 45.0)")
 
-//let res11 = evaluater("(+ (* 2 3 )( + 45 5))")
-//let res12 = evaluater("( * 2 3)")
-//let res13 = evaluater("( + 45 5)")
-//let res14 = evaluater("( * )")
-//let res141 = evaluater("( - 45 )")
-//let res145 = evaluater("( - 45 40)")
-//let res142 = evaluater("(/ 8 4 2)")
-//let res14_2 = evaluater("(/ 8)")
-
-//let res143 = evaluater("(* pi 4 3)") //not working
-//let res144 = evaluater("(print 4)")
-//let res145 = evaluater("(print hello)") // not working
-
-//let res15 = evaluater("(> 45 5)")
-//let res16 = evaluater("(<= 44 45)")
-//let res17 = evaluater("(   sqrt  49)   ")
-//let res18 = evaluater("(+ 10 (sqrt 100))")
-//let res19 = evaluater("(+ (+ 4 5) (- 16 4))")
-//let res20 = evaluater("(abs -10.5)")
-//let res21 = evaluater("(!== 45 45.0)")
-
+//let res1441 = evaluateExp("(print HELLO)")// not working
